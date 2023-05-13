97e9d468248dc17cc466b41336efc0fa0104769b
Teaom <dclarkjenkins87@gmail.com>
2023-05-13 12:34:51 -0600

    Brought over files

---
+++ models/Course.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+const { User } = require('.');
+
+// Schema to create a course model
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/\w+@\w+.\{2,3}+/, "Not valid email"]
+    },
+    students: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Student',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Course = model('user', userSchema);
+
+module.exports = User;
