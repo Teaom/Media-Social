1071e41094d8ac16edad60d41b10e4c13f773065
Teaom <dclarkjenkins87@gmail.com>
2023-05-13 18:05:02 -0600

    spelling check

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,6 @@ const userRoutes = require('./userRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
 
 router.use('/users', userRoutes);
-router.use('/thoguths', thoughtRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
